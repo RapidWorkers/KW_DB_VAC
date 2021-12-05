38955877d0ae3f9140c94214dde0096e9cbb30aa
heejin1998 <bobae1998@naver.com>
2021-12-05 19:50:18 +0900

    Update report js ejs

--- webapps/routes/report.js
+++ webapps/routes/report.js
@@ -12,23 +12,38 @@ else
 });
 
 /* POST home page. */
-router.post('/', function(req, res, next) {
-
-  var sqlInsertReport = "INSERT INTO VACC_REPORT(reserve_id, report_type, report_time) VALUES(?, ?, ?)"
-if(req.session.loggedin === undefined || req.session.loggedin ===0)
-  res.send("<script>alert('로그인이 필요합니다.');location.href='login';</script>");
-else{
-  const {report} = req.body;
-  console.log(report);
-  try{
-    var conn = getSqlConnectionAsync();
-    conn.release();
-    res.render('report_result', { title: '이상반응 신고 완료', loggedin: 1, legal_name: req.session.legal_name});
-  }catch(err){
-
-  }
+router.post('/', async function(req, res, next) {
   
-}
+  var sqlGetReserveId = "SELECT id FROM RESERVATION WHERE uid=? ORDER BY current_series DESC;";
+  var sqlInsertReport = "INSERT INTO VACC_REPORT(reserve_id, report_type, report_time) VALUES(?, ?, ?);";
+  
+  if(req.session.loggedin === undefined || req.session.loggedin ===0)
+    res.send("<script>alert('로그인이 필요합니다.');location.href='login';</script>");
+  else{
+    req.body = JSON.parse(JSON.stringify(req.body));
+    const {report} = req.body;
+
+    var today = new Date();
+    var todayDate = today.getFullYear() + "-" + (today.getMonth()+1) + "-" + today.getDate()+" "+today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
+    
+    
+    try{
+      var conn = await getSqlConnectionAsync();
+      [row, fields] = await conn.query(sqlGetReserveId, [req.session.uid]);
+
+      var len = Object.keys(report).length;
+      for(var i=0;i<len;i++){
+        [rows, fields] = await conn.query(sqlInsertReport, [row[0].id, report[i], todayDate]);
+      }
+           
+      conn.release();
+      res.render('report_result', { title: '이상반응 신고완료' , loggedin: 1, legal_name: req.session.legal_name});      
+    }catch(err){
+      console.log("Error: MySQL returned ERROR : " + err);
+      conn.release();
+    }
+    
+  }
   
 });
 
