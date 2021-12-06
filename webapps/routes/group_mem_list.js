b79a23ab5a16395362904b2150f35291a26da7fb
heejin1998 <bobae1998@naver.com>
2021-12-06 23:33:48 +0900

    Update group_mem_list js ejs

--- webapps/routes/group_mem_list.js
+++ webapps/routes/group_mem_list.js
@@ -7,14 +7,21 @@ router.get('/:gid', async function(req, res, next) {
   if(req.session.loggedin === 1)
   {
     // TEAM_MEM에서 gid인 사람의 uid들을 전부 불러와서 USER 테이블에서 정보 얻어오기
-    var sqlGetTeam = "SELECT id, team_name, profile_img FROM TEAM WHERE id=?;";
+    var sqlGetTeam = "SELECT * FROM TEAM WHERE id=? and owner_uid = ?;";
     var sqlGetMemList = "SELECT * FROM USER WHERE uid IN (SELECT uid FROM TEAM_MEM T WHERE gid = ?);";
+    
     try{
       var conn = await getSqlConnectionAsync();
 
-      var [groups,fields] = await conn.query(sqlGetTeam, [req.params.gid]);
-      var [rows, fields] = await conn.query(sqlGetMemList,[req.params.gid]);
+      var [groups,fields] = await conn.query(sqlGetTeam, [req.params.gid, req.session.uid]);
 
+      if(groups.length == 0)
+      {
+        conn.release();
+        return res.send('<script>alert("잘못된 경로로 접근하였습니다.");location.href="/group_list";</script>'); 
+      }
+
+      var [rows, fields] = await conn.query(sqlGetMemList,[req.params.gid]);
       var renderInfo={
         title: '그룹 구성원 목록 보기',
         loggedin: 1,
@@ -24,10 +31,9 @@ router.get('/:gid', async function(req, res, next) {
        
       }
       
-      
       conn.release();
-
       res.render('group_mem_list', renderInfo);
+
     }catch(err){
       console.log("Error: MySQL returned ERROR :" + err);
       conn.release();
