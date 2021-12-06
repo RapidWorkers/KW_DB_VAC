82808fb9d2f41f5e2aa3ca4c044e768c89249f72
Yuha0123 <bbkjo0123@gmail.com>
2021-12-06 23:40:22 +0900

    백신 예약 날짜 선택 페이지
    구현

--- webapps/routes/reserve_select_date.js
+++ webapps/routes/reserve_select_date.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
+
 router.post('/', function(req, res, next) {
   if(req.session.loggedin === undefined  || req.session.loggedin ===0){
     res.send("<script>alert('로그인이 필요합니다.');location.href='login';</script>");
@@ -10,10 +11,12 @@ router.post('/', function(req, res, next) {
 
      //해당 병원에서 예약할 수 있는 백신 정보를 가져와 화면에 출력
     var hospital_id = req.body.hospital_id;
-    var hospital_name, reserve_date, vac_id, vac_name;
+    var hospital_name = req.body.hospital_name;
+    //reserve_date, vac_id, vac_name;
     
     //sql
     var sqlGetHospitalName = "SELECT name FROM HOSPITAL WHERE id=?";
+    
 
     //날짜 선택
 
@@ -22,8 +25,8 @@ router.post('/', function(req, res, next) {
       title: '예약 날짜 선택',
       legal_name: req.session.legal_name,
       loggedin: 1,
-      hospital_id,
-      hospital_name
+      hospital_id : hospital_id,
+      hospital_name: hospital_name
     };
 
     res.render('reserve_select_date', renderInfo);
