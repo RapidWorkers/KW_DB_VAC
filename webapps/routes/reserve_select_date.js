f4aecce2995ccc3268eaf9a4ef70edc6078a1de1
asdfg20564 <44521546+asdfg20564@users.noreply.github.com>
2021-12-06 13:02:35 +0900

    modify reserve_select_date
    modify reserve_select_date

--- webapps/routes/reserve_select_date.js
+++ webapps/routes/reserve_select_date.js
@@ -5,16 +5,25 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
   if(req.session.loggedin === undefined  || req.session.loggedin ===0){
     res.send("<script>alert('로그인이 필요합니다.');location.href='login';</script>");
-    var happy= req.query.body.hospital_name;
   }
   else{
-    var hospital_name = req.body.hospital_name;
 
+     //해당 병원에서 예약할 수 있는 백신 정보를 가져와 화면에 출력
+    var hospital_id = req.body.hospital_id;
+    var hospital_name, reserve_date, vac_id, vac_name;
+    
+    //sql
+    var sqlGetHospitalName = "SELECT name FROM HOSPITAL WHERE id=?";
+
+    //날짜 선택
+
+    //예약
     var renderInfo = {
       title: '예약 날짜 선택',
       legal_name: req.session.legal_name,
       loggedin: 1,
-      hospital_name: hospital_name
+      hospital_id,
+      hospital_name
     };
 
     res.render('reserve_select_date', renderInfo);
