eca92871de073d0bddb6a48d27b078b9fe5d752a
SOOHYUN KIM <RapidWorkers@users.noreply.github.com>
2021-12-08 20:32:56 +0900

    fix: api router missing link
    fix: api router missing link

--- webapps/routes/api/api_router.js
+++ webapps/routes/api/api_router.js
@@ -6,11 +6,13 @@ var validate_join = require('./validate_join');
 var profile_image_upload = require('./profile_image_upload');
 var group_api = require('./group_api');
 var group_profile_image_upload = require('./group_profile_image_upload');
+var get_vaccine_num_api = require('./get_vaccine_num_api');
 
 router.use('/get_hospital_list_by_area', get_hospital_list_by_area);
 router.use('/validate_join', validate_join);
 router.use('/profile_image_upload', profile_image_upload);
 router.use('/group_api', group_api);
 router.use('/group_profile_image_upload', group_profile_image_upload);
+router.use('/get_vaccine_num_api', get_vaccine_num_api);
 
 module.exports = router;
