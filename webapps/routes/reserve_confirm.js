af5c7ee77e1f71fba1a0dc35087b411a809b1622
SOOHYUN KIM <RapidWorkers@users.noreply.github.com>
2021-12-09 23:00:14 +0900

    Impl: change reserve

--- webapps/routes/reserve_confirm.js
+++ webapps/routes/reserve_confirm.js
@@ -19,6 +19,7 @@ router.post('/', async function(req, res, next) {
     var hospital_name = req.body.hospital_name;
     var reserve_date = req.body.reserve_date;
     var uid = req.session.uid;
+    var reserveChange = req.body.reserveChange;
 
     if(!vaccine_id || !vaccine_name || !hospital_id || !hospital_name || !reserve_date)
     {
@@ -29,6 +30,7 @@ router.post('/', async function(req, res, next) {
     var sqlGetVaccInfo = "SELECT min_age FROM VACCINE WHERE id = ?";
     var sqlInsertLeftVaccReserve = "INSERT INTO RESERVATION(reserve_date, uid, vaccine_type, hospital_id, current_series) VALUE(?,?,?,?,?)";
     var sqlGetReservation = "SELECT id FROM RESERVATION WHERE uid = ? and current_series = ?";
+    var sqlUpdateOldReserv = "UPDATE RESERVATION SET reserve_date = ?, vaccine_type = ?, hospital_id = ? WHERE uid = ? and current_series = ?;";
 
     try{
       var conn = await getSqlConnectionAsync();
@@ -69,19 +71,31 @@ router.post('/', async function(req, res, next) {
       if(series === 3)
         var reason = "접종 완료";
   
-      if(reserveInfo.length > 0)
+      console.log(reserveChange);
+      if(reserveInfo.length > 0 && !reserveChange)
         var reason = "기예약건 존재";
+
+      if(reserveInfo.length == 0 && reserveChange)
+        var reason = "기존 예약이 없어 예약변경 불가능"
       
       if(reason)
       {
         res.render('reserve_fail', { title: '백신 예약 실패', loggedin: 1, legal_name: req.session.legal_name, 
         reason: reason ,vaccine_name: vaccine_name, hospital_name: hospital_name});
+        conn.release();
       }
       else
       {
-        var reserveInfo = [reserve_date, uid, vaccine_id, hospital_id, series];
-
-        var [rows, fields] = await conn.query(sqlInsertLeftVaccReserve, reserveInfo);
+        if(reserveChange==1)
+        {
+          var reserveInfo = [reserve_date, vaccine_id, hospital_id, uid, series];
+          var [rows, fields] = await conn.query(sqlUpdateOldReserv, reserveInfo);
+        }
+        else
+        {
+          var reserveInfo = [reserve_date, uid, vaccine_id, hospital_id, series];
+          var [rows, fields] = await conn.query(sqlInsertLeftVaccReserve, reserveInfo);
+        }
 
         res.render('reserve_confirm', { title: '백신 예약 성공', loggedin: 1, legal_name: req.session.legal_name
         ,vaccine_name: vaccine_name, hospital_name: hospital_name});
--- webapps/routes/reserve_select_date.js
+++ webapps/routes/reserve_select_date.js
@@ -12,13 +12,15 @@ router.post('/', function(req, res, next) {
      //해당 병원에서 예약할 수 있는 백신 정보를 가져와 화면에 출력
     var hospital_id = req.body.hospital_id;
     var hospital_name = req.body.hospital_name;
+    var reserveChange = req.body.reserveChange;
    
     var renderInfo = {
       title: '예약 날짜 선택',
       legal_name: req.session.legal_name,
       loggedin: 1,
       hospital_id : hospital_id,
-      hospital_name: hospital_name
+      hospital_name: hospital_name,
+      reserveChange: reserveChange
     };
 
     res.render('reserve_select_date', renderInfo);
